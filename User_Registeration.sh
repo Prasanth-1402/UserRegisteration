60a2941e307c693041e39879fa41a585e94fcd91
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-07-14 16:33:57 +0530

    [Prasanth] Email-ID verified

--- User_Registeration.sh
+++ User_Registeration.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 read -p "Enter First Name: " Fname
 
 pat="^([A-Z]{1}[a-zA-z]{2,})"
 
 if [[ $Fname =~ $pat ]]
 then
-	 read -p "Enter First Name: " Lname
+	 read -p "Enter Last Name: " Lname
    if [[ $Lname =~ $pat ]]
    then
       echo First and Last Names are Verified and Valid
@@ -15,4 +15,17 @@ then
 else
 	echo Invalid Last Name
 fi
+
+
+read -p "Enter Email-ID : " mailID
+Emailpat="^[a-z0-9]+([.+-]{1}[a-z0-9]{3,}){0,2}\@[a-z0-9]+([.]{1}[a-z]{2,4}){1,2}$"
+
+
+if [[ $mailID =~ $Emailpat ]]
+then
+	echo Email-ID is  valid
+else
+	echo Invalid Email-ID  invalid
+fi
+
 
