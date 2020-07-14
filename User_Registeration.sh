1b875e85af46c7111202a84487564029f2e30b4a
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-07-14 16:36:50 +0530

    [Prasanth] Misspells are Rectified

--- User_Registeration.sh
+++ User_Registeration.sh
@@ -5,7 +5,7 @@ pat="^([A-Z]{1}[a-zA-z]{2,})"
 
 if [[ $Fname =~ $pat ]]
 then
-	 read -p "Enter First Name: " Lname
+	 read -p "Enter Last Name: " Lname
    if [[ $Lname =~ $pat ]]
    then
       echo First and Last Names are Verified and Valid
